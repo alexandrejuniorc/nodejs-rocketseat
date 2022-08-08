6b61fc7a5a48f6c876f40a99b07377524f007fa7
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-08-08 15:18:08 -0300

    método POST que não irá funcionar

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World Ignite - Fundamentos NodeJS!' });
+app.get('/courses', (request, response) => {
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3']);
+});
+
+// método POST que não irá funcionar na web, pois, na web ela faz somente o GET da api, assim tendo que utilizar o POSTMAN
+app.post('/courses', (request, response) => {
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
 });
 
 app.listen(3333);
