347d5f2325ce2fb6850d2539616c838eb8b0f8d3
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-08-08 16:20:55 -0300

    amostra de tipos de parâmetros utilizados

--- src/index.js
+++ src/index.js
@@ -2,20 +2,36 @@ const express = require('express');
 
 const app = express();
 
+/* tipos de parâmetros
+ * exemplo - '/courses/:id' - o :id é um Route Params
+ * Route Params => serve para identificar um recurso para poder editar/deletar
+ * Query Params => serve para criar uma paginação/filtro de busca
+ * Body Params => os objetos inserção/alteração
+ */
+
 app.get('/courses', (request, response) => {
+  const query = request.query;
+  console.log(query);
   return response.json(['Curso 1', 'Curso 2', 'Curso 3']);
 });
 
 app.post('/courses', (request, response) => {
+  const body = request.body;
+  console.log(body);
   return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
 });
 
 app.put('/courses/:id', (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
 });
 
-// o método patch ele é utilizado para atualizar apenas uma parte do objeto, mas nesse exemplo irá funcionar do mesmo jeito que o input por ser uma tarefa simples
 app.patch('/courses/:id', (request, response) => {
   return response.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4']);
+});
+
+app.delete('/courses/:id', (request, response) => {
+  return response.json(['Curso 6', 'Curso 7', 'Curso 4']);
 });
 app.listen(3333);
