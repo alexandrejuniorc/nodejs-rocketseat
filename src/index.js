3425adb0df9d7e559f890cffdd2072f5a5393140
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-08-08 15:26:40 -0300

    método patch para atualizar somente uma parte do objeto

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ app.put('/courses/:id', (request, response) => {
   return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
 });
 
+// o método patch ele é utilizado para atualizar apenas uma parte do objeto, mas nesse exemplo irá funcionar do mesmo jeito que o input por ser uma tarefa simples
 app.patch('/courses/:id', (request, response) => {
   return response.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4']);
 });
